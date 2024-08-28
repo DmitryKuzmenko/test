8312813110b1518e18b36be08bc95c74f63bc455
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2024-08-28 14:29:13 +0300

    Fixup

--- .github/actions/js-action/index.js
+++ .github/actions/js-action/index.js
@@ -9,26 +9,30 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
   try {
-    console.log(`jsn idx: Head: ${payload["head"]}`);
-    console.log(`jsn idx: Head ref: ${payload["head"]["ref"]}`);
+    console.log(`jsn idx: PR: ${payload["pull_request"]}`);
+    console.log(`jsn idx: Head: ${payload["pull_request"]["head"]}`);
+    console.log(`jsn idx: Ref: ${payload["pull_request"]["head"]["ref"]}`);
   } catch (error) {
     console.log(`Error: ${error}`);
   }
   try {
-    console.log(`jsn prp: Head: ${payload.head}`);
-    console.log(`jsn prp: Head ref: ${payload.head.ref}`);
+    console.log(`jsn prp: PR: ${payload.pull_request}`);
+    console.log(`jsn prp: Head: ${payload.pull_request.head}`);
+    console.log(`jsn prp: Ref: ${payload.pull_request.head.ref}`);
   } catch (error) {
     console.log(`Error: ${error}`);
   }
   try {
-    console.log(`ctx idx: Head: ${github.context.payload["head"]}`);
-    console.log(`ctx idx: Head ref: ${github.context.payload["head"]["ref"]}`);
+    console.log(`ctx idx: PR: ${github.context.payload["pull_request"]}`);
+    console.log(`ctx idx: Head: ${github.context.payload["pull_request"]["head"]}`);
+    console.log(`ctx idx: Ref: ${github.context.payload["pull_request"]["head"]["ref"]}`);
   } catch (error) {
     console.log(`Error: ${error}`);
   }
   try {
-    console.log(`ctx prp: Head: ${github.context.payload.head}`);
-    console.log(`ctx prp: Head ref: ${github.context.payload.head.ref}`);
+    console.log(`ctx prp: PR: ${github.context.payload.pull_request}`);
+    console.log(`ctx prp: Head: ${github.context.payload.pull_request.head}`);
+    console.log(`ctx prp: Ref: ${github.context.payload.pull_request.head.ref}`);
   } catch (error) {
     console.log(`Error: ${error}`);
   }
