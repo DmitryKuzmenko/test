2bd27b56ddd1a1374fe99495526b252bdf358539
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2024-08-28 15:27:25 +0300

    Import netlify

--- .github/actions/js-action/index.js
+++ .github/actions/js-action/index.js
@@ -1,3 +1,5 @@
+import { NetlifyAPI } from "netlify";
+
 const core = require("@actions/core");
 const github = require("@actions/github");
 
@@ -9,6 +11,9 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
   console.log(`Head Ref: ${github.context.payload.pull_request.head.ref}`);
+  const client = new NetlifyAPI("nfp_6oiUkxrMJ4Kh1zhoGTARfLAMzbtVnbPC3e67");
+  const sites = await client.listSites();
+  console.log(sites);
 } catch (error) {
   core.setFailed(error.message);
 }
