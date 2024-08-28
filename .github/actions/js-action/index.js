2c93ab1792c36e6c07541dfd22c658101e2318bb
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2024-08-28 12:50:01 +0300

    Fixup

--- .github/actions/js-action/index.js
+++ .github/actions/js-action/index.js
@@ -1,14 +1,14 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('name');
+  const nameToGreet = core.getInput("name");
   console.log(`Hello ${nameToGreet}!`);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
-  console.log(`Head ref: ${github.context.payload.head.ref}`)
+  console.log(`Head ref: ${github.context.payload["head"]["ref"]}`);
 } catch (error) {
   core.setFailed(error.message);
 }
