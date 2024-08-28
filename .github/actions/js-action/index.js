be82525fbdc57da84e225b526f1ec8ebf92c7495
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2024-08-28 13:10:39 +0300

    Debug

--- .github/actions/js-action/index.js
+++ .github/actions/js-action/index.js
@@ -8,7 +8,30 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
-  console.log(`Head ref: ${payload.head.ref}`);
+  try {
+    console.log(`jsn idx: Head: ${payload["head"]}`);
+    console.log(`jsn idx: Head ref: ${payload["head"]["ref"]}`);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+  try {
+    console.log(`jsn prp: Head: ${payload.head}`);
+    console.log(`jsn prp: Head ref: ${payload.head.ref}`);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+  try {
+    console.log(`ctx idx: Head: ${github.context.payload["head"]}`);
+    console.log(`ctx idx: Head ref: ${github.context.payload["head"]["ref"]}`);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+  try {
+    console.log(`ctx prp: Head: ${github.context.payload.head}`);
+    console.log(`ctx prp: Head ref: ${github.context.payload.head.ref}`);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
