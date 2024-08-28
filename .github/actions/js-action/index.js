11d600ffbfeb2e3eb2ddac4e91d3d5d81bffe321
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2024-08-28 14:33:11 +0300

    cleanup

--- .github/actions/js-action/index.js
+++ .github/actions/js-action/index.js
@@ -8,34 +8,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
-  try {
-    console.log(`jsn idx: PR: ${payload["pull_request"]}`);
-    console.log(`jsn idx: Head: ${payload["pull_request"]["head"]}`);
-    console.log(`jsn idx: Ref: ${payload["pull_request"]["head"]["ref"]}`);
-  } catch (error) {
-    console.log(`Error: ${error}`);
-  }
-  try {
-    console.log(`jsn prp: PR: ${payload.pull_request}`);
-    console.log(`jsn prp: Head: ${payload.pull_request.head}`);
-    console.log(`jsn prp: Ref: ${payload.pull_request.head.ref}`);
-  } catch (error) {
-    console.log(`Error: ${error}`);
-  }
-  try {
-    console.log(`ctx idx: PR: ${github.context.payload["pull_request"]}`);
-    console.log(`ctx idx: Head: ${github.context.payload["pull_request"]["head"]}`);
-    console.log(`ctx idx: Ref: ${github.context.payload["pull_request"]["head"]["ref"]}`);
-  } catch (error) {
-    console.log(`Error: ${error}`);
-  }
-  try {
-    console.log(`ctx prp: PR: ${github.context.payload.pull_request}`);
-    console.log(`ctx prp: Head: ${github.context.payload.pull_request.head}`);
-    console.log(`ctx prp: Ref: ${github.context.payload.pull_request.head.ref}`);
-  } catch (error) {
-    console.log(`Error: ${error}`);
-  }
+  console.log(`Head Ref: ${github.context.payload.pull_request.head.ref}`);
 } catch (error) {
   core.setFailed(error.message);
 }
