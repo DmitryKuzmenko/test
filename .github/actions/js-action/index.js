576b594c996e0bbced4791856c9bd7c817aba3fd
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2024-08-28 11:47:31 +0300

    Test payload head ref

--- .github/actions/js-action/index.js
+++ .github/actions/js-action/index.js
@@ -8,6 +8,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  console.log(`Head ref: ${github.context.payload.head.ref}`)
 } catch (error) {
   core.setFailed(error.message);
 }
