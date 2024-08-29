5b4f111697e02e8feb11a336720f5955cdb3af65
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2024-08-29 13:42:33 +0300

    Update

--- .github/actions/js-action/index.js
+++ .github/actions/js-action/index.js
@@ -14,6 +14,25 @@ try {
   const client = new NetlifyAPI("nfp_6oiUkxrMJ4Kh1zhoGTARfLAMzbtVnbPC3e67");
   const sites = await client.listSites();
   console.log(sites);
+
+  console.log("Trying HTTP post");
+  let xhr = new XMLHttpRequest();
+  xhr.open(
+    "POST",
+    "https://httpbin.org/post?trigger_branch=GITHUB_HEAD_REF&trigger_title=TITLE"
+  );
+  xhr.setRequestHeader("Content-Type", "applicathion/json");
+  xhr.send(
+    JSON.stringify({
+      rscEnv: "STAGE",
+      cognitoUserPoolId: "COGNITO_USER_POOL_ID",
+      cognitoClientId: "COGNITO_CLIENT_ID",
+    })
+  );
+  xhr.onload = function() {
+    let resp = xhr.response;
+    console.log(`HTTP Response: ${resp.message}`)
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
